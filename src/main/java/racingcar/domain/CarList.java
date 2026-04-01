787bcbda086c9c78f3b9debf753283030cac1f73
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-04-01 18:03:41 +0900

    feat: CarList 클래스 구현

---
+++ src/main/java/racingcar/domain/CarList.java
@@ -0,0 +1,42 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarList {
+    private final List<Car> carList;
+
+    public CarList(List<Car> carList) {
+        this.carList = carList;
+    }
+
+    public void playRoundAllCar(Race race) {
+        for (Car car : carList) {
+            race.playRound(car);
+        }
+    }
+
+    public List<Car> findWinner() {
+        int maxPosition = 0;
+        for (Car car : carList) {
+            int curPosition = car.getPosition();
+            if (curPosition > maxPosition) {
+                maxPosition = curPosition;
+            }
+        }
+
+        List<Car> winners = new ArrayList<>();
+        for (Car car : carList) {
+            int curPosition = car.getPosition();
+            if (curPosition == maxPosition) {
+                winners.add(car);
+            }
+        }
+
+        return winners;
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+}
