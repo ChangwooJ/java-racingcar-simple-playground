82fb4647964b7373860b776fd4dedd9eaca39f4a
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-04-08 23:43:45 +0900

    fix: Random 인스턴스 재사용

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class RandomNumberGenerator implements NumberGenerator {
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 10;
+    private final Random random = new Random();
 
     @Override
     public int generateRandomNumber() {
-        return new Random().nextInt(MIN_NUMBER, MAX_NUMBER);
+        return random.nextInt(MIN_NUMBER, MAX_NUMBER);
     }
 }
