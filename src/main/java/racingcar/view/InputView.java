266ae1163e2be63d4ded1869afcbe6a1cf58e188
ChangwooJ <127019318+ChangwooJ@users.noreply.github.com>
2026-05-14 14:47:16 +0900

    refactor: 시도 횟수 메시지 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,15 +6,6 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputNumberOfCars() {
-        System.out.println("출전시킬 자동차의 대수를 입력해주세요: ");
-
-        int numberOfCars = scanner.nextInt();
-        scanner.nextLine();
-
-        return numberOfCars;
-    }
-
     public static String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
@@ -22,7 +13,7 @@ public class InputView {
     }
 
     public static int inputRoundNumber() {
-        System.out.println("반복할 라운드 수를 입력해주세요: ");
+        System.out.println("시도할 획수는 몇회인가요?");
         return scanner.nextInt();
     }
 }
